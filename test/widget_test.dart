06a191c0dffa375966b4fd4068b3988e51b0f34c
heimin22 <fyketonel@gmail.com>
2024-11-27 18:04:21 +0800

    Added the fonts, dependencies.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:pasada_passenger_app/main.dart';
+import 'package:pasada_passenger_app/loginScreen.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const PassengerPassengerApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
