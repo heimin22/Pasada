47fcfe5598ea29bf9365d7b7df17a4ffc9f180e8
heimin22 <fyketonel@gmail.com>
2024-11-28 19:35:51 +0800

    The text fields so far, incomplete. Still working on the functions.

--- lib/createAccount.dart
+++ lib/createAccount.dart
@@ -6,24 +6,26 @@ void main() => runApp(const CreateAccountPage());
 
 class CreateAccountPage extends StatelessWidget {
   const CreateAccountPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Create Account',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        scaffoldBackgroundColor: const Color(0xFFF2F2F2),
-        fontFamily: 'Inter',
-        useMaterial3: true,
-      ),
-      home: const CAPage(title: 'Create Account')
-    );
+        title: 'Create Account',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          scaffoldBackgroundColor: const Color(0xFFF2F2F2),
+          fontFamily: 'Inter',
+          useMaterial3: true,
+        ),
+        home: const CAPage(title: 'Create Account'));
   }
 }
 
 class CAPage extends StatefulWidget {
   const CAPage({super.key, required this.title});
+
   final String title;
+
   @override
   State<CAPage> createState() => CreateAccountScreen();
 }
@@ -32,13 +34,17 @@ class CreateAccountScreen extends State<CAPage> {
   @override
   Widget build(BuildContext context) {
     final TextEditingController inputController = TextEditingController();
+    final TextEditingController passwordController = TextEditingController();
+    bool isPasswordVisible = false;
+
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Container(
-              margin: const EdgeInsets.only(bottom: 30.0, right: 300.0),
+              margin:
+                  const EdgeInsets.only(bottom: 30.0, right: 300.0, top: 120.0),
               height: 80,
               width: 80,
               child: SvgPicture.asset('assets/svg/Ellipse.svg'),
@@ -59,13 +65,12 @@ class CreateAccountScreen extends State<CAPage> {
               margin: const EdgeInsets.only(bottom: 28.0, right: 60.0),
               child: const Text(
                 'Join the Pasada app and make your ride easier',
-                style: TextStyle(
-                  color: Color(0xFF121212)
-                ),
+                style: TextStyle(color: Color(0xFF121212)),
               ),
             ),
             Container(
-              margin: const EdgeInsets.only(left: 36.0, right: 39.0, bottom: 380.0),
+              margin:
+                  const EdgeInsets.only(left: 35.0, right: 39.0, bottom: 25.0),
               height: 50,
               child: TextField(
                 controller: inputController,
@@ -75,14 +80,10 @@ class CreateAccountScreen extends State<CAPage> {
                 decoration: const InputDecoration(
                   labelText: 'Enter your email or phone number',
                   labelStyle: TextStyle(
-                    fontSize: 14,
+                    fontSize: 12,
                   ),
                   floatingLabelStyle: TextStyle(
-                    color: Color(0xFF4AB00C)
-                  ),
-                  hintText: 'Email or phone number',
-                  hintStyle: TextStyle(
-                    fontSize: 4,
+                    color: Color(0xFF4AB00C),
                   ),
                   enabledBorder: OutlineInputBorder(
                     borderSide: BorderSide(
@@ -97,22 +98,61 @@ class CreateAccountScreen extends State<CAPage> {
                     ),
                   ),
                 ),
-                keyboardType: TextInputType.emailAddress,
               ),
             ),
             Container(
-              margin: const EdgeInsets.only(left: 36.0, right: 39.0),
-              height: 50,
-              child: const TextField(
+              margin: const EdgeInsets.only(bottom: 20.0, right: 307.0),
+              child: const Text(
+                'Password',
                 style: TextStyle(
+                  color: Color(0xFF121212),
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ),
+            Container(
+              margin:
+                  const EdgeInsets.only(left: 35.0, right: 39.0, bottom: 450.0),
+              height: 50,
+              child: TextField(
+                controller: passwordController,
+                obscureText: !isPasswordVisible,
+                style: const TextStyle(
                   color: Color(0xFF121212),
                 ),
                 decoration: InputDecoration(
-                  hintText: 'Enter your password',
-                  hintStyle: TextStyle(
-                    fontSize: 4,
+                  labelText: 'Enter your password',
+                  suffixIcon: IconButton(
+                    color: const Color(0xFF121212),
+                    onPressed: () {
+                      setState(() {
+                        isPasswordVisible = !isPasswordVisible;
+                      });
+                    },
+                    icon: Icon(
+                      isPasswordVisible
+                          ? Icons.visibility
+                          : Icons.visibility_off,
+                    ),
                   ),
-                  labelText: 'Enter your password'
+                  labelStyle: const TextStyle(
+                    fontSize: 12,
+                  ),
+                  floatingLabelStyle: const TextStyle(
+                    color: Color(0xFF4AB00C),
+                  ),
+                  enabledBorder: const OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color(0xFFC7C7C6),
+                      width: 1.0,
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  ),
+                  focusedBorder: const OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color(0xFF4AB00C),
+                    ),
+                  ),
                 ),
               ),
             ),
@@ -121,18 +161,18 @@ class CreateAccountScreen extends State<CAPage> {
       ),
     );
   }
-  
-  // bool validateEmailOrPhone(String input) {
-  //   final emailRegex = RegExp(r"^[^\s@]+@[^\s@]+\.[^\s@]+$");
-  //   if (emailRegex.hasMatch(input)) {
-  //     return true;
-  //   }
-  //
-  //   final phoneRegex = RegExp(r"^\d{10,}$");
-  //   if (phoneRegex.hasMatch(input)) {
-  //     return true;
-  //   }
-  //
-  //   return false;
-  // }
+
+// bool validateEmailOrPhone(String input) {
+//   final emailRegex = RegExp(r"^[^\s@]+@[^\s@]+\.[^\s@]+$");
+//   if (emailRegex.hasMatch(input)) {
+//     return true;
+//   }
+//
+//   final phoneRegex = RegExp(r"^\d{10,}$");
+//   if (phoneRegex.hasMatch(input)) {
+//     return true;
+//   }
+//
+//   return false;
+// }
 }
