047b161499ca5c8497ebd1cc9136e23e5805fd53
heimin22 <fyketonel@gmail.com>
2024-11-28 16:36:16 +0800

    goods na yung isang text field

--- lib/createAccount.dart
+++ lib/createAccount.dart
@@ -37,15 +37,60 @@ class CreateAccountScreen extends State<CAPage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Container(
-              margin: const EdgeInsets.only(bottom: 200.0),
+              margin: const EdgeInsets.only(bottom: 30.0, right: 300.0),
+              height: 80,
+              width: 80,
+              child: SvgPicture.asset('assets/svg/Ellipse.svg'),
+            ),
+            Container(
+              margin: const EdgeInsets.only(bottom: 10.0, right: 134.0),
               child: const Text(
                 'Create your account',
                 style: const TextStyle(
                   color: Color(0xFF121212),
-                  fontWeight: FontWeight.w600,
-                  fontSize: 20,
+                  fontWeight: FontWeight.w700,
+                  fontSize: 24,
                 ),
               ),
+            ),
+            const SizedBox(height: 3),
+            Container(
+              margin: const EdgeInsets.only(bottom: 28.0, right: 60.0),
+              child: const Text(
+                'Join the Pasada app and make your ride easier',
+                style: TextStyle(
+                  color: Color(0xFF121212)
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(left: 37.0, right: 70.0, bottom: 380.0),
+              height: 50,
+              child: const TextField(
+                style: TextStyle(
+                  color: Color(0xFF121212),
+                ),
+                decoration: InputDecoration(
+                  hintText: 'Email or phone number',
+                  hintStyle: TextStyle(
+                    fontSize: 4,
+                  ),
+                  labelText: 'Enter email or phone number',
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(
+                      color: Color(0xFFC7C7C6),
+                      width: 1.0,
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  ),
+                ),
+                keyboardType: TextInputType.emailAddress,
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(left: 37.0, right: 70.0),
+              height: 50,
+              child: const TextField(),
             ),
           ],
         ),
