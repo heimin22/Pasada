c5de5128ad3c39a0a49f5300b91af068ed6fefca
heimin22 <fyketonel@gmail.com>
2024-12-01 16:12:31 +0800

    google button and second text field is done. the back button has functions

--- lib/loginAccount.dart
+++ lib/loginAccount.dart
@@ -10,18 +10,18 @@ class LoginAccountPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Log-in',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          scaffoldBackgroundColor: const Color(0xFFF2F2F2),
-          fontFamily: 'Inter',
-          useMaterial3: true,
-        ),
-        home: const LoginPage(title: 'Log-in to your account'),
-        routes: <String, WidgetBuilder>{
-
-        }
-        );
+      title: 'Log-in',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color(0xFFF2F2F2),
+        fontFamily: 'Inter',
+        useMaterial3: true,
+      ),
+      home: const LoginPage(title: 'Log-in to your account'),
+      routes: <String, WidgetBuilder>{
+        'start': (BuildContext context) => const PasadaPassenger(),
+      },
+    );
   }
 }
 
@@ -51,7 +51,9 @@ class LoginScreen extends State<LoginPage> {
             Container(
               margin: const EdgeInsets.only(top: 35.0, right: 360.0),
               child: IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.pushNamed(context, 'start');
+                },
                 icon: Icon(Icons.arrow_back),
               ),
             ),
@@ -62,7 +64,7 @@ class LoginScreen extends State<LoginPage> {
               child: SvgPicture.asset('assets/svg/Ellipse.svg'),
             ),
             Padding(
-              padding: EdgeInsets.only(bottom: 10.0, right: 134.0),
+              padding: EdgeInsets.only(bottom: 25.0, right: 63.0),
               child: Text(
                 'Enter your email or mobile number to continue',
                 style: TextStyle(color: Color(0xFF121212)),
@@ -166,6 +168,50 @@ class LoginScreen extends State<LoginPage> {
                   style: TextStyle(
                     color: Color(0xFFF2F2F2),
                     fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(bottom: 5.0, top: 75.0),
+              child: SvgPicture.asset('assets/svg/otherOptionsOptimized.svg'),
+            ),
+            Container(
+              // Google Button
+              margin: EdgeInsets.only(bottom: 20.0),
+              child: Center(
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(
+                    maxWidth: 360,
+                  ),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF4AB00C),
+                      minimumSize: const Size(360, 50),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      )
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SvgPicture.asset(
+                          'assets/svg/googleIcon.svg',
+                          height: 24,
+                          width: 24,
+                        ),
+                        const SizedBox(width: 25),
+                        const Text(
+                          'Sign-up with Google',
+                          style: TextStyle(
+                            color: Color(0xFFF2F2F2),
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
