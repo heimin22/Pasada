321989deb3f37243692342ebc5174c23c6f64961
heimin22 <fyketonel@gmail.com>
2025-03-31 20:14:10 +0800

    - Revamped bottom navigation bar.
    NOTE:
    - Adding a Confirm Booking button.
    - Changing navigation bar into a curved one.
    - Adding some functions in the settings.
    - Editing the profile.
    - Push notifications.
    - Booking functions.
    - Setting preferences.

--- lib/home/selectionScreen.dart
+++ lib/home/selectionScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 // import 'package:url_launcher/url_launcher.dart';
 // import 'package:location/location.dart';
 // import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -16,6 +17,7 @@ class selectionScreen extends StatefulWidget {
 
 class selectionState extends State<selectionScreen> {
   int _currentIndex = 0;
+  final GlobalKey<CurvedNavigationBarState> _bottomNavigationKey = GlobalKey();
 
   final List<Widget> pages = const [
     HomeScreen(),
@@ -29,63 +31,64 @@ class selectionState extends State<selectionScreen> {
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xFFF2F2F2),
-      body: IndexedStack( // Use IndexedStack to preserve state
-        index: _currentIndex,
-        children: pages,
-      ),
-      bottomNavigationBar: _buildBottomNavBar(),
-    );
-  }
-
-  BottomNavigationBar _buildBottomNavBar() {
-    return BottomNavigationBar(
-      backgroundColor: const Color(0xFFF2F2F2),
-      currentIndex: _currentIndex,
-      onTap: onTap,
-      showSelectedLabels: true,
-      showUnselectedLabels: false,
-      selectedLabelStyle: const TextStyle(
-        color: Color(0xFF121212),
-        fontFamily: 'Inter',
-        fontSize: 12,
-      ),
-      unselectedLabelStyle: const TextStyle(
-        fontFamily: 'Inter',
-        fontSize: 12,
-      ),
-      selectedItemColor: const Color(0xff067837),
-      type: BottomNavigationBarType.fixed,
-      items: [
-        _buildNavItem(0, 'Home', 'homeSelectedIcon.svg', 'homeIcon.svg'),
-        _buildNavItem(1, 'Activity', 'activitySelectedIcon.svg', 'activityIcon.svg'),
-        _buildNavItem(2, 'Account', 'profileSelectedIcon.svg', 'profileIcon.svg'),
+  Widget buildCurvedNavItem(int index) {
+    String selectedIcon, unselectedIcon;
+    switch (index) {
+      case 0:
+        selectedIcon = 'homeSelectedIcon.svg';
+        unselectedIcon = 'homeIcon.svg';
+        break;
+      case 1:
+        selectedIcon = 'activitySelectedIcon.svg';
+        unselectedIcon = 'activityIcon.svg';
+        break;
+      case 2:
+        selectedIcon = 'profileSelectedIcon.svg';
+        unselectedIcon = 'profileIcon.svg';
+        break;
+      default:
+        selectedIcon = '';
+        unselectedIcon = '';
+    }
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        SvgPicture.asset(
+          'assets/svg/${_currentIndex == index ? selectedIcon : unselectedIcon}',
+          colorFilter: _currentIndex == index
+              ? ColorFilter.mode(Color(0xff067837), BlendMode.srcIn)
+              : null,
+          width: 24,
+          height: 24,
+        ),
       ],
     );
   }
 
-  BottomNavigationBarItem _buildNavItem(
-      int index,
-      String label,
-      String selectedIcon,
-      String unselectedIcon
-      ) {
-    return BottomNavigationBarItem(
-      label: label,
-      icon: _currentIndex == index
-          ? SvgPicture.asset(
-        'assets/svg/$selectedIcon',
-        colorFilter: ColorFilter.mode(Color(0xff067837), BlendMode.srcIn),
-        width: 24,
-        height: 24,
-      )
-          : SvgPicture.asset(
-        'assets/svg/$unselectedIcon',
-        width: 24,
-        height: 24,
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFF5F5F5),
+      body: IndexedStack(
+        index: _currentIndex,
+        children: pages,
+      ),
+      bottomNavigationBar: CurvedNavigationBar(
+        key: _bottomNavigationKey,
+        items: [
+          buildCurvedNavItem(0),
+          buildCurvedNavItem(1),
+          buildCurvedNavItem(2),
+        ],
+        index: _currentIndex,
+        color: const Color(0xFF00CC58),
+        backgroundColor: Color(0xFFF5F5F5),
+        buttonBackgroundColor: Color(0xFFF5F5F5),
+
+        onTap: onTap,
+        animationCurve: Curves.easeOutCubic,
+        animationDuration: const Duration(milliseconds: 400),
+        height: 75.0,
       ),
     );
   }
