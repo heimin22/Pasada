346978941b22359f1b55bbeb8247aab60dc97b3e
heimin22 <fyketonel@gmail.com>
2025-04-03 22:42:09 +0800

    CONTRIBUTIONS: - Moving some files to another folders and organizing - Adding H3 Hexagonal Indexing System for geohashing. - Working on driverService.dart now. - Working on localDatabaseService.dart
    RESULTS:
    
    NOTE:
    - Adding some functions in the settings.
    - Editing the profile.
    - Push notifications.
    - Booking functions.
    - Setting preferences.

--- lib/services/localDatabaseService.dart
+++ lib/services/localDatabaseService.dart
@@ -8,6 +8,20 @@ class LocalDatabaseService {
   static const String dbName = 'passenger_bookings.db';
   static const String tableName = 'bookings';
 
+  Future<Database> get BookingDatabase async {
+    if (database != null) return database!;
+    database = await initDatabase();
+    return database!;
+  }
 
+  Future<Database> initDatabase() async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, dbName);
 
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: onCreate,
+    );
+  }
 }
