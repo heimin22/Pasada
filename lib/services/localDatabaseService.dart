b0a159b94cb85145e60ec0a0d9c6ce1329bf363e
heimin22 <fyketonel@gmail.com>
2025-04-03 22:48:25 +0800

    CONTRIBUTIONS: - Moving some files to another folders and organizing - Adding H3 Hexagonal Indexing System for geohashing. - Working on driverService.dart now. - Working on localDatabaseService.dart
    RESULTS:
    
    NOTE:
    - Adding some functions in the settings.
    - Editing the profile.
    - Push notifications.
    - Booking functions.
    - Setting preferences.

--- lib/services/localDatabaseService.dart
+++ lib/services/localDatabaseService.dart
@@ -24,4 +24,28 @@ class LocalDatabaseService {
       onCreate: onCreate,
     );
   }
+
+  Future<void> onCreate(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE $tableName (
+        booking_id INTEGER PRIMARY KEY,
+        driver_id INTEGER NOT NULL,
+        route_id INTEGER NOT NULL,
+        ride_status TEXT NOT NULL CHECK(ride_status IN ('searching', 'assigned', 'in_progress', 'completed', 'cancelled', 'no_driver')),
+        pickup_address TEXT NOT NULL,
+        pickup_lat REAL NOT NULL,
+        pickup_lng REAL NOT NULL,
+        dropoff_address TEXT NOT NULL,
+        dropoff_lat REAL NOT NULL,
+        dropoff_lng REAL NOT NULL,
+        start_time TEXT NOT NULL,
+        created_at TEXT NOT NULL,
+        fare REAL NOT NULL,
+        assigned_at TEXT NOT NULL,
+        end_time TEXT NOT NULL,
+        passenger_id TEXT NOT NULL
+      )
+    ''');
+    debugPrint("Local SQLite table '$tableName' created.");
+  }
 }
