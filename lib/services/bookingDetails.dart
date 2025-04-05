dfccc1e122b42d7f7e248ad232facbb495721125
heimin22 <fyketonel@gmail.com>
2025-04-05 21:45:51 +0800

    CONTRIBUTIONS: - Updated the Gradle support.
    RESULTS:
    
    NOTE:
    - Adding some functions in the settings.
    - Editing the profile.
    - Push notifications.
    - Booking functions.
    - Setting preferences.

--- lib/services/bookingDetails.dart
+++ lib/services/bookingDetails.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter/foundation.dart';
 
@@ -11,11 +12,11 @@ class BookingDetails {
   final LatLng pickupCoordinates;
   final String dropoffAddress;
   final LatLng dropoffCoordinates;
-  final String startTime;
+  final TimeOfDay startTime;
   final DateTime createdAt;
   final double fare;
   final DateTime assignedAt;
-  final String endTime;
+  final TimeOfDay endTime;
 
   BookingDetails({
     required this.bookingId,
@@ -47,11 +48,11 @@ class BookingDetails {
       'dropoff_address': dropoffAddress,
       'dropoff_lat': dropoffCoordinates.latitude.toString(),
       'dropoff_lng': dropoffCoordinates.longitude.toString(),
-      'start_time': startTime,
+      'start_time': '${startTime.hour}:${startTime.minute}',
       'created_at': createdAt.toIso8601String(),
       'fare': fare,
       'assigned_at': assignedAt.toIso8601String(),
-      'end_time': endTime,
+      'end_time': '${startTime.hour}:${startTime.minute}',
     };
   }
 
