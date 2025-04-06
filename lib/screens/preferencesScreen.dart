4aae064a07987a326b3211e392ec1aafd520d901
heimin22 <fyketonel@gmail.com>
2025-04-06 18:41:39 +0800

    CONTRIBUTIONS: - Working on preferencesScreen.dart
    RESULTS:
    
    NOTE:
    - Adding some functions in the settings.
    - Editing the profile.
    - Push notifications.
    - Booking functions.
    - Setting preferences.

--- lib/screens/preferencesScreen.dart
+++ lib/screens/preferencesScreen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:pasada_passenger_app/services/authService.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:pasada_passenger_app/functions/notification_preferences.dart';
 
 class PreferencesScreen extends StatefulWidget {
@@ -11,30 +13,73 @@ class PreferencesScreen extends StatefulWidget {
 
 class PreferencesScreenState extends State<PreferencesScreen> {
   bool notificationsEnabled = true;
+  final AuthService authService = AuthService();
 
   @override
   void initState() {
     super.initState();
+    loadNotificationPreferences();
+  }
+
+  Future<void> loadNotificationPreferences() async {
+    final enabled = await NotificationPreference.getNotificationStatus();
+    setState(() => notificationsEnabled = enabled);
+  }
+
+  Future<void> toggleNotification(bool value) async {
+    await NotificationPreference.setNotificationStatus(value);
+    setState(() => notificationsEnabled = value);
   }
 
 
   @override
   Widget build(BuildContext context) {
+    // final screenHeight = MediaQuery.of(context).size.height;
+    // final screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Preferences',
-          style: TextStyle(
-            fontFamily: 'Inter',
-            fontSize: 18,
-            fontWeight: FontWeight.w700,
-            color: Color(0xFF121212),
-          ),
+      appBar: buildAppBar(),
+      body: (),
+    );
+  }
+
+  PreferredSizeWidget buildAppBar() {
+    return AppBar(
+      title: const Text(
+        'Preferences',
+        style: TextStyle(
+          fontFamily: 'Inter',
+          fontSize: 18,
+          fontWeight: FontWeight.w700,
+          color: Color(0xFF121212),
         ),
-        backgroundColor: Color(0xFFF5F5F5),
-        foregroundColor: Color(0xFF121212),
-        elevation: 1.0,
       ),
+      backgroundColor: Color(0xFFF5F5F5),
+      foregroundColor: Color(0xFF121212),
+      elevation: 1.0,
+    );
+  }
+
+  Widget buildBody(Size screenSize) {
+    return SafeArea(
+      child: Column(
+        children: [
+          buildNotificationSection(screenSize.width),
+          buildAppSettingsSection(screenSize.width),
+        ],
+      ),
+    );
+  }
+
+  Widget buildNotificationSection(double screenWidth) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.03),
+    );
+  }
+
+  Widget buildAppSettingsSection(double screenWidth) {
+    return Padding(
+
     );
   }
 }
