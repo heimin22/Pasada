91e47aa66f1e42b16459a3e398561f1f44a2dd90
heimin22 <fyketonel@gmail.com>
2025-04-10 05:16:07 +0800

    Now working on editing the profile screen

---
+++ lib/screens/editProfileScreen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:pasada_passenger_app/services/authService.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+
+class EditProfileScreen extends StatefulWidget {
+  const EditProfileScreen({super.key});
+
+  @override
+  State<EditProfileScreen> createState() => _EditProfileScreenState();
+}
+
+class _EditProfileScreenState extends State<EditProfileScreen> {
+  final AuthService authService = AuthService();
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController mobileNumberController = TextEditingController();
+  String? profileImageUrl;
+  File? _imageFile;
+  bool isGoogleLinked = false;
+
+  @override
+  void initState() {
+    super.initState();
+    // Fetch user data and initialize controllers
+    loadUserData();
+  }
+
+  Future<void> loadUserData() async {
+    final userData = await authService.getCurrentUserData();
+    if (userData != null) {
+      setState(() {
+        nameController.text = userData['display_name'] ?? '';
+        emailController.text = userData['passenger_email'] ?? '';
+        mobileNumberController.text = userData['contact_number'] ?? '';
+        profileImageUrl = userData['avatar_url'];
+        isGoogleLinked = userData['is_google_linked'] ?? false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
