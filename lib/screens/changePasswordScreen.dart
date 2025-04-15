294724b02c8ceecb385945c3c9b35c905774bc43
heimin22 <fyketonel@gmail.com>
2025-04-15 19:12:25 +0800

    Now working on changing the passwrod right now

---
+++ lib/screens/changePasswordScreen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:pasada_passenger_app/services/authService.dart';
+import 'package:pasada_passenger_app/theme/theme_controller.dart';
+import 'package:pasada_passenger_app/profiles/theme_preferences.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter/cupertino.dart';
+
+class ChangePasswordScreen extends StatefulWidget {
+  const ChangePasswordScreen({super.key});
+
+  @override
+  State<ChangePasswordScreen> createState() => ChangePasswordScreenState();
+}
+
+class ChangePasswordScreenState extends State<ChangePasswordScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
