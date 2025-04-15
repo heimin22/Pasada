09394c5ce1acf895a123184ea965cfaa6251e4c2
heimin22 <fyketonel@gmail.com>
2025-04-15 19:50:33 +0800

    Now working on the text fields

--- lib/screens/changePasswordScreen.dart
+++ lib/screens/changePasswordScreen.dart
@@ -14,6 +14,14 @@ class ChangePasswordScreen extends StatefulWidget {
 
 class ChangePasswordScreenState extends State<ChangePasswordScreen> {
   final AuthService authService = AuthService();
+  final TextEditingController currentPasswordController =
+      TextEditingController();
+  final TextEditingController newPasswordController = TextEditingController();
+  final TextEditingController confirmPasswordController =
+      TextEditingController();
+  bool isPasswordVisible = false;
+  bool isLoading = false;
+  String errorMessage = '';
 
   @override
   void initState() {
@@ -22,6 +30,9 @@ class ChangePasswordScreenState extends State<ChangePasswordScreen> {
 
   @override
   void dispose() {
+    currentPasswordController.dispose();
+    newPasswordController.dispose();
+    confirmPasswordController.dispose();
     super.dispose();
   }
 
@@ -41,6 +52,46 @@ class ChangePasswordScreenState extends State<ChangePasswordScreen> {
       foregroundColor:
           isDarkMode ? const Color(0xFFF5F5F5) : const Color(0xFF121212),
       elevation: 1.0,
+    );
+  }
+
+  Container buildPasswordField(
+      String label, TextEditingController controller, bool isDarkMode) {
+    return Container(
+      margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.02),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            label,
+            style: TextStyle(
+              color: isDarkMode
+                  ? const Color(0xFFF5F5F5)
+                  : const Color(0xFF121212),
+              fontWeight: FontWeight.w600,
+              fontFamily: 'Inter',
+            ),
+          ),
+          const SizedBox(height: 8),
+          SizedBox(
+            width: double.infinity,
+            height: 45,
+            child: TextField(
+              controller: controller,
+              obscureText: !isPasswordVisible,
+              style: TextStyle(
+                color: isDarkMode
+                    ? const Color(0xFFF5F5F5)
+                    : const Color(0xFF121212),
+                fontWeight: FontWeight.w600,
+                fontFamily: 'Inter',
+                fontSize: 14,
+              ),
+              decoration: InputDecoration(),
+            ),
+          ),
+        ],
+      ),
     );
   }
 
