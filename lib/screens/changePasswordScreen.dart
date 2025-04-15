c9ab341fed1390d30c856ed2c91e5f5057ea9ff1
heimin22 <fyketonel@gmail.com>
2025-04-15 19:19:01 +0800

    App bar for the change password screen

--- lib/screens/changePasswordScreen.dart
+++ lib/screens/changePasswordScreen.dart
@@ -13,6 +13,37 @@ class ChangePasswordScreen extends StatefulWidget {
 }
 
 class ChangePasswordScreenState extends State<ChangePasswordScreen> {
+  final AuthService authService = AuthService();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  PreferredSizeWidget buildAppBar(bool isDarkMode) {
+    return AppBar(
+      title: Text(
+        'Change Password',
+        style: TextStyle(
+          fontFamily: 'Inter',
+          fontSize: 18,
+          fontWeight: FontWeight.w700,
+          color: isDarkMode ? const Color(0xFFF5F5F5) : const Color(0xFF121212),
+        ),
+      ),
+      backgroundColor:
+          isDarkMode ? const Color(0xFF121212) : const Color(0xFFF5F5F5),
+      foregroundColor:
+          isDarkMode ? const Color(0xFFF5F5F5) : const Color(0xFF121212),
+      elevation: 1.0,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
