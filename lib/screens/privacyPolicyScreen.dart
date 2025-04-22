282188f052330c556baed5043bc7d3a8d287ce0a
heimin22 <fyketonel@gmail.com>
2025-04-22 19:23:08 +0800

    Creating the privacy policy screen right now (app bar)

--- lib/screens/privacyPolicyScreen.dart
+++ lib/screens/privacyPolicyScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
 
 class PrivacyPolicyScreen extends StatefulWidget {
   const PrivacyPolicyScreen({super.key});
@@ -11,7 +13,7 @@ class _PrivacyPolicyScreenState extends State<PrivacyPolicyScreen> {
   PreferredSizeWidget buildAppBar(bool isDarkMode) {
     return AppBar(
       title: Text(
-        'Change Password',
+        'Privacy Policy',
         style: TextStyle(
           fontFamily: 'Inter',
           fontSize: 18,
@@ -29,6 +31,30 @@ class _PrivacyPolicyScreenState extends State<PrivacyPolicyScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final isDarkMode = Theme.of(context).brightness == Brightness.dark;
+    return Scaffold(
+      appBar: buildAppBar(isDarkMode),
+      backgroundColor:
+          isDarkMode ? const Color(0xFF121212) : const Color(0xFFF5F5F5),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Last Updated: 22 April 2025',
+              style: TextStyle(
+                fontFamily: 'Inter',
+                fontSize: 14,
+                fontWeight: FontWeight.w500,
+                color: isDarkMode
+                    ? const Color(0xFFF5F5F5)
+                    : const Color(0xFF121212),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
