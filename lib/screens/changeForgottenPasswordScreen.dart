d65eb5496b95411af0b897f6aada8936d0b05fd5
heimin22 <fyketonel@gmail.com>
2025-04-23 01:22:18 +0800

    Function codes are now done. Implementing the user interface right now

--- lib/screens/changeForgottenPasswordScreen.dart
+++ lib/screens/changeForgottenPasswordScreen.dart
@@ -4,7 +4,9 @@ import 'package:fluttertoast/fluttertoast.dart';
 import 'package:pasada_passenger_app/screens/selectionScreen.dart';
 
 class ChangeForgottenPasswordScreen extends StatefulWidget {
-  const ChangeForgottenPasswordScreen({super.key});
+  final String email;
+
+  const ChangeForgottenPasswordScreen({super.key, required this.email});
 
   @override
   State<ChangeForgottenPasswordScreen> createState() =>
@@ -19,7 +21,43 @@ class ChangeForgottenPasswordScreenState
   final AuthService _authService = AuthService();
   bool _isLoading = false;
 
-  Future<void> handleForgotPassword() async {}
+  Future<void> handleForgotPassword() async {
+    if (_newPasswordController.text.isEmpty ||
+        _confirmPasswordController.text.isEmpty) {
+      Fluttertoast.showToast(
+        msg: "Please fill in all fields",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        backgroundColor: Color(0xFFF5F5F5),
+        textColor: Color(0xFF121212),
+      );
+      return;
+    }
+
+    setState(() => _isLoading = true);
+
+    try {
+      await _authService.login(widget.email, _newPasswordController.text);
+      if (mounted) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => selectionScreen()),
+        );
+      }
+    } catch (e) {
+      if (mounted) {
+        Fluttertoast.showToast(
+          msg: 'Failed to change password',
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          backgroundColor: Color(0xFFF5F5F5),
+          textColor: Color(0xFF121212),
+        );
+      }
+    } finally {
+      if (mounted) setState(() => _isLoading = false);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
