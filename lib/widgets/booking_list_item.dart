780b844dea19e62cabcb657e167b58e839e21e22
heimin22 <fyketonel@gmail.com>
2025-04-28 12:47:45 +0800

    UI ng widget

--- lib/widgets/booking_list_item.dart
+++ lib/widgets/booking_list_item.dart
@@ -24,6 +24,63 @@ class BookingListItem extends StatelessWidget {
           ),
         ),
       ),
+      child: Row(
+        children: [
+          Container(
+            padding: EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: const Color(0xFF00CC58),
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Icon(
+              Icons.directions_car_outlined,
+              color: const Color(0xFF00CC58),
+              size: 24,
+            ),
+          ),
+          SizedBox(width: 15),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '${booking['pickup_address']} to ${booking['dropoff_address']}',
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w600,
+                    color: isDarkMode
+                        ? const Color(0xFFF5F5F5)
+                        : const Color(0xFF121212),
+                  ),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                SizedBox(height: 4),
+                Text(
+                  '$formattedDate, $formattedTime',
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: isDarkMode
+                        ? const Color(0xFFAAAAAA)
+                        : const Color(0xFF515151),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(width: 15),
+          Text(
+            '₱${booking['fare'].toStringAsFixed(2)}',
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w600,
+              color: isDarkMode
+                  ? const Color(0xFFF5F5F5)
+                  : const Color(0xFF121212),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
