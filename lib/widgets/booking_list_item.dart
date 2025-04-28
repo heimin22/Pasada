82f18e1d0d38d2d3abba3753094c8bf6dc2357cb
heimin22 <fyketonel@gmail.com>
2025-04-28 12:36:36 +0800

    Setting up the preliminary codes for the list item widget

--- lib/widgets/booking_list_item.dart
+++ lib/widgets/booking_list_item.dart
@@ -8,6 +8,22 @@ class BookingListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final isDarkMode = Theme.of(context).brightness == Brightness.dark;
+    final createdAt = DateTime.parse(booking['created_at']);
+    final formattedDate = DateFormat('MMM d, yyyy').format(createdAt);
+    final formattedTime = DateFormat('h:mm a').format(createdAt);
+
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            color:
+                isDarkMode ? const Color(0xFF2A2A2A) : const Color(0xFFE0E0E0),
+            width: 1,
+          ),
+        ),
+      ),
+    );
   }
 }
