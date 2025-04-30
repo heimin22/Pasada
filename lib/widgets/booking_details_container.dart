2fceacd0bcdcda26570706a81cc96ec776b68a56
heimin22 <fyketonel@gmail.com>
2025-04-30 08:25:56 +0800

    Styling the booking details container right now

--- lib/widgets/booking_details_container.dart
+++ lib/widgets/booking_details_container.dart
@@ -20,6 +20,36 @@ class BookingDetailsContainer extends StatelessWidget {
     final now = DateTime.now();
     final formattedTime = DateFormat('h:mm a').format(now);
 
-    return Container();
+    return Container(
+      padding: const EdgeInsets.all(16),
+      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      decoration: BoxDecoration(
+        color: isDarkMode ? const Color(0xFF1E1E1E) : const Color(0xFFF5F5F5),
+        borderRadius: BorderRadius.circular(8),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 10,
+            spreadRadius: 0,
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Booking Details',
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w700,
+              color: isDarkMode
+                  ? const Color(0xFFF5F5F5)
+                  : const Color(0xFF121212),
+            ),
+          ),
+          const SizedBox(height: 16),
+        ],
+      ),
+    );
   }
 }
