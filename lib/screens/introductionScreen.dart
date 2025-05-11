2e961876ae0f166c66bc1d7253236a7b2bb2b055
heimin22 <fyketonel@gmail.com>
2025-05-12 06:14:11 +0800

    ito na nagcocode na

--- lib/screens/introductionScreen.dart
+++ lib/screens/introductionScreen.dart
@@ -8,6 +8,52 @@ class IntroductionScreen extends StatefulWidget {
 }
 
 class _IntroductionScreenState extends State<IntroductionScreen> {
+  late AnimationController _animationController;
+  late Animation<Offset> _slideAnimation;
+  late Animation<double> _fadeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1200),
+    );
+
+    _fadeAnimation = Tween<double>(
+      begin: 0,
+      end: 1,
+    ).animate(
+        CurvedAnimation(parent: _animationController, curve: Curves.easeOut));
+
+    _slideAnimation = Tween<Offset>(
+      begin: const Offset(0, 0.3),
+      end: Offset.zero,
+    ).animate(
+        CurvedAnimation(parent: _animationController, curve: Curves.easeOut));
+
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  String _getTimeBasedGreeting() {
+    final hour = DateTime.now().hour;
+    if (hour >= 5 && hour < 12) {
+      return 'Magandang Umaga!';
+    } else if (hour >= 12 && hour < 18) {
+      return 'Magandang Hapon!';
+    } else if (hour >= 18 && hour < 22) {
+      return 'Magandang Gabi!';
+    } else {
+      return 'Magandang Gabi!';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
