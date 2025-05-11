e77fa1c18ff25a1ae18421e99226a0a5ee9ed9a8
heimin22 <fyketonel@gmail.com>
2025-05-12 06:17:10 +0800

    ito na yung buo

--- lib/screens/introductionScreen.dart
+++ lib/screens/introductionScreen.dart
@@ -7,7 +7,8 @@ class IntroductionScreen extends StatefulWidget {
   State<IntroductionScreen> createState() => _IntroductionScreenState();
 }
 
-class _IntroductionScreenState extends State<IntroductionScreen> {
+class _IntroductionScreenState extends State<IntroductionScreen>
+    with SingleTickerProviderStateMixin {
   late AnimationController _animationController;
   late Animation<Offset> _slideAnimation;
   late Animation<double> _fadeAnimation;
@@ -20,17 +21,14 @@ class _IntroductionScreenState extends State<IntroductionScreen> {
       duration: const Duration(milliseconds: 1200),
     );
 
-    _fadeAnimation = Tween<double>(
-      begin: 0,
-      end: 1,
-    ).animate(
-        CurvedAnimation(parent: _animationController, curve: Curves.easeOut));
+    _fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.easeOut),
+    );
 
-    _slideAnimation = Tween<Offset>(
-      begin: const Offset(0, 0.3),
-      end: Offset.zero,
-    ).animate(
-        CurvedAnimation(parent: _animationController, curve: Curves.easeOut));
+    _slideAnimation =
+        Tween<Offset>(begin: const Offset(0, 0.3), end: Offset.zero).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.easeOut),
+    );
 
     _animationController.forward();
   }
@@ -54,8 +52,83 @@ class _IntroductionScreenState extends State<IntroductionScreen> {
     }
   }
 
+  LinearGradient _getTimeBasedGradient() {
+    final hour = DateTime.now().hour;
+    if (hour >= 5 && hour < 12) {
+      // Morning
+      return const LinearGradient(
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        colors: [Color(0xFF236078), Color(0xFF439464)],
+        transform: GradientRotation(21 * 3.14159 / 180),
+      );
+    } else if (hour >= 12 && hour < 18) {
+      // Afternoon
+      return const LinearGradient(
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        colors: [Color(0xFFCFA425), Color(0xFF26AB37)],
+        transform: GradientRotation(21 * 3.14159 / 180),
+      );
+    } else if (hour >= 18 && hour < 22) {
+      // Evening
+      return const LinearGradient(
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        colors: [Color(0xFFB45F4F), Color(0xFF705776)],
+        transform: GradientRotation(21 * 3.14159 / 180),
+      );
+    } else {
+      // Night
+      return const LinearGradient(
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        colors: [Color(0xFF2E3B4E), Color(0xFF1C1F2E)],
+        transform: GradientRotation(21 * 3.14159 / 180),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: _getTimeBasedGradient(),
+        ),
+        child: Center(
+          child: SlideTransition(
+            position: _slideAnimation,
+            child: FadeTransition(
+              opacity: _fadeAnimation,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    _getTimeBasedGreeting(),
+                    style: const TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.w700,
+                      color: Color(0xFFF5F5F5),
+                      fontFamily: 'Inter',
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  const Text(
+                    'Welcome sa Pasada!',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 20,
+                      color: Color(0xFFF5F5F5),
+                      fontFamily: 'Inter',
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
